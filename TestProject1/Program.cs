63240657b1e71ff3f4bdc1798f8c94c939d0d389
mkalme <mikelis.kalme@gmail.com>
2025-05-22 18:09:48 +0300

    Add project files.

---
+++ TestProject1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int offset = 2;
+
+            int j = offset >> 8;
+            int k = offset & 255;
+
+            Console.WriteLine(j + " | " + k);
+
+            Console.ReadLine();
+        }
+    }
+}
